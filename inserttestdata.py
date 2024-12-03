cd2a43b9d955ecc5b08b22b7d13811b0e1f5f7fe
Sho <harimoto@ulaberta.ca>
2024-12-03 12:53:49 -0700

    changes in inserttestdata

--- inserttestdata.py
+++ inserttestdata.py
@@ -4,7 +4,11 @@ from pymongo import MongoClient
 # Connect to MongoDB
 client = MongoClient('mongodb://localhost:27017/')
 db = client['291db']
-tweets = db['tweets']
+mycoll =  db.list_collection_names()
+if 'tweets' in mycoll:
+    db.drap_collection('tweets')
+else:
+    tweets = db['tweets']
 
 # Load the JSON file
 with open('100.json', 'r', encoding='utf-8') as file:
